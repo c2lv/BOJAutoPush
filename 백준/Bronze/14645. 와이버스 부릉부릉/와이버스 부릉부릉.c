2eea945f6be9bcd3740dec58a66a52e3e2543676
Hyeonjun Park <50288201+c2lv@users.noreply.github.com>
2023-03-10 22:00:25 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int N, K, A, B;
+
+	scanf("%d %d\n", &N, &K);
+	for (int i = 1; i < N; i++)
+	{
+		scanf("%d %d\n", &A, &B);
+	}
+	printf("비와이");
+	return 0;
+}
