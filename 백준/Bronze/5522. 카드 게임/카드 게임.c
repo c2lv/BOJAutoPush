5ce62def2f0306e8dca235d11cc55b2d58913eb9
Hyeonjun Park <50288201+c2lv@users.noreply.github.com>
2023-03-10 21:44:50 +0900

    [Bronze V] Title: 카드 게임, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int g1, g2, g3, g4, g5;
+
+	scanf("%d", &g1);
+	scanf("%d", &g2);
+	scanf("%d", &g3);
+	scanf("%d", &g4);
+	scanf("%d", &g5);
+	printf("%d", g1 + g2 + g3 + g4 + g5);
+	return 0;
+}
