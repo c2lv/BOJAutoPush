b03d86c83818df9313416a7b324115ab5d1c695f
Hyeonjun Park <50288201+c2lv@users.noreply.github.com>
2023-03-10 21:44:38 +0900

    [Bronze V] Title: 콜센터, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+	// 백슬래시(\)는 특수 문자이자 다음 문자가 특수 문자임을 알리는 문자이다.
+	// 따라서 백슬래시를 출력하기 위해서는 앞에 백슬래시를 추가해주어야 한다.
+	printf("     /~\\\n");
+	printf("    ( oo|\n");
+	printf("    _\\=/_\n");
+	printf("   /  _  \\\n");
+	printf("  //|/.\\|\\\\\n");
+	printf(" ||  \\ /  ||\n");
+	printf("============\n");
+	printf("|          |\n");
+	printf("|          |\n");
+	printf("|          |");
+	return 0;
+}
