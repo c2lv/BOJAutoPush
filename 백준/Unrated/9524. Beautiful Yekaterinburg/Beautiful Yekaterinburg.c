45ccecd11e58fe00302728ea72c563a715aa5398
Hyeonjun Park <50288201+c2lv@users.noreply.github.com>
2023-03-10 21:49:15 +0900

    [Unrated] Title: Beautiful Yekaterinburg, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Unrated/9524. Beautiful Yekaterinburg/Beautiful Yekaterinburg.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int D;
+
+    scanf("%d", &D);
+
+    switch(D)
+    {
+        case 1:
+            printf("1");
+            break;
+        case 2:
+            printf("7");
+            break;
+        case 3:
+            printf("2");
+            break;
+        case 4:
+            printf("3");
+            break;
+    }
+
+    return 0;
+}
