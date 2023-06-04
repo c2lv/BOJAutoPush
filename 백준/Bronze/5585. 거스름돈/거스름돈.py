f0e76d5fd5b9bcabe005e3f4ab437adbac1147cc
Hyeonjun Park <50288201+c2lv@users.noreply.github.com>
2023-06-04 21:53:37 +0900

    [Bronze II] Title: 거스름돈, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/5585. 거스름돈/거스름돈.py
+++ 백준/Bronze/5585. 거스름돈/거스름돈.py
@@ -1,14 +1,19 @@
-import sys
+# 물건의 가격 입력 받기
+price = int(input())
 
-data = sys.stdin.readline().rstrip()
-n = 1000 - int(data)
-count = 0
+# 지불액과 잔돈 계산
+payment = 1000
+change = payment - price
 
-# 큰 단위의 화폐부터 차례대로 확인하기
-array = [500, 100, 50, 10, 5, 1]
+# 잔돈의 종류와 개수를 저장하는 리스트
+coins = [500, 100, 50, 10, 5, 1]
+coin_count = 0  # 잔돈의 개수 초기화
 
-for coin in array:
-    count += n // coin # 해당 화폐로 거슬러 줄 수 있는 동전의 개수 세기
-    n %= coin
+# 잔돈의 개수 계산
+for coin in coins:
+    count = change // coin  # 현재 동전으로 거슬러 줄 수 있는 개수 계산
+    change -= count * coin  # 거슬러 준 돈만큼 잔돈에서 차감
+    coin_count += count  # 잔돈의 개수에 더하기
 
-print(count)
+# 잔돈 개수 출력
+print(coin_count)
