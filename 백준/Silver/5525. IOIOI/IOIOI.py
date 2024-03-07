f095565700de95a3ee09cdca38e73b874752481a
Hyeonjun Park <50288201+c2lv@users.noreply.github.com>
2024-03-07 11:02:31 +0900

    [Silver I] Title: IOIOI, Time: 376 ms, Memory: 33076 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -5,8 +5,21 @@ n = int(input())
 m = int(input())
 s = input()
 
+i_start = False
+diff = 0
 cnt = 0
-for i in range(m - 2*n):
-    if s[i:2*n+1+i] == "I"+"OI"*n:
-        cnt += 1
+
+for i in range(m):
+    if i_start:
+        if s[i-1] != s[i]:
+            diff += 1
+            if s[i] == 'I' and diff // 2 >= n:
+                cnt += 1
+        else:
+            diff = 0
+            if s[i] == 'O':
+                i_start = False
+    else:
+        if s[i] == 'I':
+            i_start = True
 print(cnt)
