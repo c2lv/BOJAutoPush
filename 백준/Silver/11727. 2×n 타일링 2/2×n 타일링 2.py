6b99c53bfa7df8051e8d77dea80f5c1ac16eb829
Hyeonjun Park <50288201+c2lv@users.noreply.github.com>
2024-03-07 10:08:29 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 56 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
@@ -0,0 +1,15 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+def solve(n):
+    if n == 1:
+        return 1
+    dp = [0] * (n+1)
+    dp[1] = 1
+    dp[2] = 3
+    for i in range(2, n):
+        dp[i+1] = (dp[i] + 2*dp[i-1]) % 10007
+    return dp[n]
+
+n = int(input())
+print(solve(n))
