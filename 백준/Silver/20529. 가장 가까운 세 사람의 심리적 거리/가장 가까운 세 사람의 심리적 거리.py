e7a5426a06e14f53f59b1348f23bef9de6068d8a
Hyeonjun Park <50288201+c2lv@users.noreply.github.com>
2024-03-07 12:23:16 +0900

    [Silver I] Title: 가장 가까운 세 사람의 심리적 거리, Time: 256 ms, Memory: 38968 KB -BaekjoonHub

--- 백준/Silver/20529. 가장 가까운 세 사람의 심리적 거리/가장 가까운 세 사람의 심리적 거리.py
+++ 백준/Silver/20529. 가장 가까운 세 사람의 심리적 거리/가장 가까운 세 사람의 심리적 거리.py
@@ -4,33 +4,19 @@ input = lambda: sys.stdin.readline().rstrip()
 t = int(input())
 for _ in range(t):
     n = int(input())
-    students = list(input().split())
-    mbti = {}
-    for student in students:
-        cases = [
-            student,
-            student[0] + student[1] + student[2],
-            student[0] + student[1] + student[3],
-            student[0] + student[2] + student[3],
-            student[1] + student[2] + student[3],
-            student[0] + student[1],
-            student[0] + student[2],
-            student[0] + student[3],
-            student[1] + student[2],
-            student[1] + student[3],
-            student[2] + student[3],
-            student[0],
-            student[1],
-            student[2],
-            student[3]
-        ]
-        for case in cases:
-            if case in mbti:
-                mbti[case] += 1
-            else:
-                mbti[case] = 1
-    max_len = 0
-    for key, value in mbti.items():
-        if value >= 3:
-            max_len = max(len(key), max_len)
-    print(8 - 2*max_len)
+    s = list(input().split())
+    if n > 32:
+        print(0)
+    else:
+        same_len = 0
+        for i in range(n-2):
+            for j in range(i+1, n-1):
+                for k in range(j+1, n):
+                    same_len = max(
+                        same_len,
+                        (s[i][0] == s[j][0] == s[k][0])
+                        + (s[i][1] == s[j][1] == s[k][1])
+                        + (s[i][2] == s[j][2] == s[k][2])
+                        + (s[i][3] == s[j][3] == s[k][3])
+                    )
+        print(8 - same_len*2)
