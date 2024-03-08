ed30909a6e5cbfae3bd65d9de20783e971d4202e
Hyeonjun Park <50288201+c2lv@users.noreply.github.com>
2024-03-08 09:18:01 +0900

    [Silver II] Title: N과 M (12), Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.py
@@ -0,0 +1,20 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+def solve(a1, a2):
+    if len(a1) == m:
+        print(*a1)
+    else:
+        for i in range(len(a2)):
+            if i > 0 and a2[i-1] == a2[i]:
+                continue
+            if len(a1) == 0 or (a1[-1] <= a2[i]): 
+                a3 = a1[:]
+                a4 = a2[:]
+                a3.append(a2[i])
+                solve(a3, a4)
+
+n, m = map(int, input().split())
+a = list(map(int, input().split()))
+a.sort()
+solve([], a)
