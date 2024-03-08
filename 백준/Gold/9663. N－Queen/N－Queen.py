4bac1fcf6ac76a2adbed2bcbc79a6435519a6d51
Hyeonjun Park <50288201+c2lv@users.noreply.github.com>
2024-03-08 15:53:16 +0900

    [Gold IV] Title: N-Queen, Time: 25084 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,17 +1,23 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
 def solve(y):
+    global case
+
     if y == n:
-        return 1
+        case += 1
+        return
     
-    case = 0
     for x in range(n):
-        if column[x] == xy_sum[x+y] == xy_sub[x-y] == 1:
+        if column[x] and xy_sum[x+y] and xy_sub[x-y]:
             column[x] = xy_sum[x+y] = xy_sub[x-y] = 0
-            case += solve(y+1)
+            solve(y+1)
             column[x] = xy_sum[x+y] = xy_sub[x-y] = 1
-    return case
 
 n = int(input())
+case = 0
 column = [1 for _ in range(n)]
 xy_sum = [1 for _ in range(2*n-1)]
 xy_sub = [1 for _ in range(2*n-1)]
-print(solve(0))
+solve(0)
+print(case)
