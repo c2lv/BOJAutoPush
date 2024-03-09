c24cbb37f2871a9f1f35c2efc34b46a714fd5b99
Hyeonjun Park <50288201+c2lv@users.noreply.github.com>
2024-03-09 09:50:02 +0900

    [Silver I] Title: 곱셈, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.py
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -1,12 +1,11 @@
 a, b, c = map(int, input().split())
 
-def ft_pow(a, b):
-    if b == 1:
-        return a
-    x = ft_pow(a, b//2)
+res = 1
+while b > 0:
     if b & 1:
-        return x * x * a % c
-    else:
-        return x * x % c
-
-print(ft_pow(a, b) % c)
+        res *= a % c
+        res %= c
+    a *= a
+    a %= c
+    b >>= 1
+print(res % c)
