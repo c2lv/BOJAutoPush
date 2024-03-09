6b8cc46dd096eb4277b3f9854d8bbc154f49b511
Hyeonjun Park <50288201+c2lv@users.noreply.github.com>
2024-03-09 13:22:13 +0900

    [Gold II] Title: 피보나치 수 6, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.py
+++ 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.py
@@ -2,20 +2,22 @@ import sys
 input = lambda: sys.stdin.readline().rstrip()
 
 def cal_m(a, b):
+    if a == []:
+        return b
     c = [[0, 0], [0, 0]]
     for i in range(2):
         for j in range(2):
             c[i][j] = (a[i][0]*b[0][j] + a[i][1]*b[1][j]) % 1000000007
     return c
 
-def fibo(n, m):
-    if n < 2:
-        return m
-    l = cal_m(m, m)
-    if n & 1:
-        return cal_m(fibo(n//2, l), m)
-    else:
-        return fibo(n//2, l)
-
+m = [[1, 1], [1, 0]]
 n = int(input())
-print(fibo(n, [[1, 1], [1, 0]])[0][1])
+res = []
+
+while n > 1:
+    if n & 1:
+        res = cal_m(res, m)
+    m = cal_m(m, m)
+    n >>= 1
+res = cal_m(res, m)
+print(res[0][1])
