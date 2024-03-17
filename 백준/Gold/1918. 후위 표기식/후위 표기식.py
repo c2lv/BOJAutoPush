d530a69febd4e90d264666f008a92f94bed33af8
Hyeonjun Park <50288201+c2lv@users.noreply.github.com>
2024-03-18 01:21:59 +0900

    [Gold II] Title: 후위 표기식, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1918. 후위 표기식/후위 표기식.py
+++ 백준/Gold/1918. 후위 표기식/후위 표기식.py
@@ -2,55 +2,25 @@ import sys
 input = lambda: sys.stdin.readline().rstrip()
 
 infix = input()
-operator = []
-postfix = ""
-before_op = 0
-parentheses = []
+stack = []
 
 for c in infix:
-    if c == "(":
-        before_op = 0
-        parentheses.append(0)
-    elif c == ")":
-        temp = parentheses.pop()
-        while temp > 0:
-            postfix += operator.pop()
-            temp -= 1
-        if operator:
-            if operator[-1] in "+-":
-                before_op = 1
-            else:
-                before_op = 2
+    if c == '(':
+        stack.append(c)
+    elif c == ')':
+        while stack[-1] != '(':
+            print(stack.pop(), end='')
+        stack.pop()
+    elif c in "+-*/":
+        if c in "+-":
+            while stack and stack[-1] in "+-*/":
+                print(stack.pop(), end='')
         else:
-            before_op = 0
-    elif c in "+-":
-        if before_op >= 1:
-            if parentheses:
-                while parentheses[-1] > 0:
-                    postfix += operator.pop()
-                    parentheses[-1] -= 1
-            else:
-                while operator:
-                    postfix += operator.pop()
-        operator.append(c)
-        before_op = 1
-        if parentheses:
-            parentheses[-1] += 1
-    elif c in "*/":
-        if before_op == 2:
-            if parentheses:
-                while parentheses[-1] > 0 and operator[-1] in "*/":
-                    postfix += operator.pop()
-                    parentheses[-1] -= 1
-            else:
-                while operator and operator[-1] in "*/":
-                    postfix += operator.pop()
-        operator.append(c)
-        before_op = 2
-        if parentheses:
-            parentheses[-1] += 1
+            while stack and stack[-1] in "*/":
+                print(stack.pop(), end='')
+        stack.append(c)
     else:
-        postfix += c
-while operator:
-    postfix += operator.pop()
-print(postfix)
+        print(c, end='')
+
+while stack:
+    print(stack.pop(), end='')
