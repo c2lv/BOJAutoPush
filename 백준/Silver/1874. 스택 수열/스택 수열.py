67684b7c71e56c6ba6375b63b6f5228e33adfc77
Hyeonjun Park <50288201+c2lv@users.noreply.github.com>
2024-03-18 01:59:21 +0900

    [Silver II] Title: 스택 수열, Time: 2008 ms, Memory: 39448 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.py
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -4,22 +4,21 @@ input = lambda: sys.stdin.readline().rstrip()
 n = int(input())
 a = []
 stack = []
-s = ""
+s = []
 for _ in range(n):
     a.append(int(input()))
 k = 1
 while True:
     if a[0] >= k:
         stack.append(k)
-        s += "+"
+        s.append("+")
         k += 1
     elif a[0] == stack[-1]:
         stack.pop()
         a.pop(0)
-        s += "-"
+        s.append("-")
         if not a:
-            for op in s:
-                print(op)
+            print(*s, sep='\n')
             break
     else:
         print("NO")
