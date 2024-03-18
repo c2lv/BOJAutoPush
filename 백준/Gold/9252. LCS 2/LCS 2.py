31d2e1d5ca292cbbe64ba3d51be5839782bf86d6
Hyeonjun Park <50288201+c2lv@users.noreply.github.com>
2024-03-18 19:29:38 +0900

    [Gold IV] Title: LCS 2, Time: 560 ms, Memory: 55708 KB -BaekjoonHub

---
+++ 백준/Gold/9252. LCS 2/LCS 2.py
@@ -0,0 +1,32 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+s = []
+s.append(input())
+s.append(input())
+
+dp = [[0]*(len(s[0])+1) for _ in range(len(s[1])+1)]
+for i in range(1, len(s[1])+1):
+    for j in range(1, len(s[0])+1):
+        if s[0][j-1] == s[1][i-1]:
+            dp[i][j] = dp[i-1][j-1] + 1
+        else:
+            dp[i][j] = max(dp[i][j-1], dp[i-1][j])
+
+lcs_len = dp[len(s[1])][len(s[0])]
+print(lcs_len)
+if lcs_len > 0:
+    lcs = ""
+    i = len(s[1])
+    j = len(s[0])
+    while lcs_len > 0:
+        if dp[i][j] == dp[i-1][j]:
+            i -= 1
+        elif dp[i][j] == dp[i][j-1]:
+            j -= 1
+        else:
+            lcs = s[0][j-1] + lcs
+            i -= 1
+            j -= 1
+            lcs_len -= 1
+    print(lcs)
