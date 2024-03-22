7d586cb8ae710b670d6ab2b9108588ecde4d7426
Hyeonjun Park <50288201+c2lv@users.noreply.github.com>
2024-03-23 06:21:01 +0900

    [Gold II] Title: 반도체 설계, Time: 168 ms, Memory: 36884 KB -BaekjoonHub

---
+++ 백준/Gold/2352. 반도체 설계/반도체 설계.py
@@ -0,0 +1,23 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+n = int(input())
+port = list(map(int, input().split()))
+
+asc = []
+max_len = 0
+for i in range(n):
+    if max_len == 0 or asc[-1][1] < port[i]:
+        asc.append([i+1, port[i]])
+        max_len += 1
+    else:
+        left = 0
+        right = max_len - 1
+        while left < right:
+            mid = (left+right)//2
+            if asc[mid][0] < i+1 and asc[mid][1] < port[i]:
+                left = mid + 1
+            else:
+                right = mid
+        asc[left] = [i+1, port[i]]
+print(max_len)
